d42f920236650d5e7b267ac06c12a856d20aa1ed
pengency-prog <pengency@gmail.com>
2026-03-23 17:07:31 +0100

    feat: add subfolder rewrite for /thedubem and update icon

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/thedubem/:path*',
+        destination: 'https://the-dubem.vercel.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
